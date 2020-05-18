8c0a70fe9e5b5ab48779f6ee22a15c905345dd55
Lucas Drapier <lucas.drapier@gmail.com>
2020-05-19 01:36:08 +0200

    opti back

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-// var request = require('request');
 const catModel = require('../models/cats');
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -23,14 +26,19 @@ router.get('/cats', function(req, res, next) {
 
 
 // route qui permet de mettre a jour les infos d'un chat
-router.put('/cats/:cat_id/updateCat', function (req, res, next) {
+router.put('/cats/cat', function (req, res, next) {
+
+  console.log('REQ PARAMS : ',req.body.id);
+  console.log('REQ BODY : ', req.body);
 
 
   catModel.findById(req.body.id, (error, cats) => {
+    
     if (error) {
       console.log('ERRORS DE FIND DE CAT : ',errors);
       res.json({ result: false, errors });
     } else {
+      //console.log(cats);
 
       cats.score = req.body.score;
 
